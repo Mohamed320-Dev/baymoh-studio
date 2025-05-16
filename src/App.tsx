5f5fbe73924214800a0292b55a2593d39c2738db
Mohamed Elmahdi <mohamed12elmahdi@gmail.com>
2025-05-16 17:40:23 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Layout } from "./layouts/Layout";
 import { Home } from "./pages/Home";
 import { ContactP } from "./pages/ContactP";
@@ -8,16 +8,14 @@ import { Studio } from "./components/Studio";
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/studio" element={<Studio />} />
-        </Route>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/studio" element={<Studio />} />
         <Route path="/contact" element={<ContactP />} />
-      </Routes>
-    </Router>
+      </Route>
+    </Routes>
   );
 };
