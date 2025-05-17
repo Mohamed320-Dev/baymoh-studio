467739a5c0b3dd341632665a8a99f4aaa5bf52a7
Mohamed Elmahdi <mohamed12elmahdi@gmail.com>
2025-05-17 09:17:52 +0100

    fin de modify

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App'
-import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from './components/ScrollToTo'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTo";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/baymoh-studio">
       <ScrollToTop />
-        <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
